6be78e826779c18600df81fe805468150114edc7
Roman Khopkin <96550493+romkakns3222@users.noreply.github.com>
2023-02-16 07:55:28 +0300

    Update Yandex.py

--- Yandex.py
+++ Yandex.py
@@ -22,13 +22,6 @@ class Yandex():
         return res
 
 
-    def get_info(self, path):
-        self.url = 'https://cloud-api.yandex.net/v1/disk/resources'
-        res = requests.get(f'{self.url}?path={path}', headers=self.get_headers())
-        return res
-
-
-
 if __name__ == '__main__':
     ya = Yandex(token)
     print(ya.get_info('Homework_16').status_code)
